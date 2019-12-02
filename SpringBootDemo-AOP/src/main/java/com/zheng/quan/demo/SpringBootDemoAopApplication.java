4da03f9321e4a9dec1ffb4328dcf441982416d65
YuTian176104 <1170286283@qq.com>
2019-12-02 20:16:32 +0800

    新增AOP模块

---
+++ SpringBootDemo-AOP/src/main/java/com/zheng/quan/demo/SpringBootDemoAopApplication.java
@@ -0,0 +1,13 @@
+package com.zheng.quan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoAopApplication.class, args);
+	}
+
+}
