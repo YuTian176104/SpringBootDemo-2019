a79c6a8e250b9b45e3a87f9a46084dc876552749
YuTian176104 <1170286283@qq.com>
2020-02-13 16:50:51 +0800

    Activiti工作流

---
+++ SpringBootDemo-Activiti/src/main/java/com/zheng/quan/demo/SpringBootDemoActivitiApplication.java
@@ -0,0 +1,13 @@
+package com.zheng.quan.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoActivitiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoActivitiApplication.class, args);
+	}
+
+}
---
+++ SpringBootDemo-Activiti/src/test/java/com/zheng/quan/demo/SpringBootDemoActivitiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zheng.quan.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoActivitiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
